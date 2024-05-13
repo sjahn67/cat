693579f5ab70d00a6b84248946e71984d277b6bc
Seongjin Ahn <sjahn67@gmail.com>
2024-05-13 21:02:55 +0900

    change hardware pwm

--- led.js
+++ led.js
@@ -1,6 +1,8 @@
-const Gpio = require('pigpio').Gpio;
-
+const Gpio = process.env.NODE_ENV !== "production" ? require("pigpio-mock").Gpio : require("pigpio").Gpio;
 const CHANGE_INTERVAL = 100; // msec
+
+const  MAX_FREQUENCE = 125000000;
+const  MAX_DUTYCYCLE = 1000000;
 
 class ledClass {
   #led;
@@ -8,14 +10,15 @@ class ledClass {
   #CurrentDutyCyle;
 
   constructor() {
-    this.#led = new Gpio(26, { mode: Gpio.OUTPUT });
+    this.#led = new Gpio(12, { mode: Gpio.OUTPUT });
     this.#TargetDutyCycle = 0;
     this.#CurrentDutyCyle = 0;
     setInterval(() => {
       if (this.#TargetDutyCycle == this.#CurrentDutyCyle) return;
       const delta = (this.#TargetDutyCycle > this.#CurrentDutyCyle) ? 1 : -1;
       this.#CurrentDutyCyle += delta;
-      this.#led.pwmWrite(this.#CurrentDutyCyle);
+      // this.#led.pwmWrite(this.#CurrentDutyCyle);
+      this.#led.hardwarePwmWrite(MAX_FREQUENCE, this.#CurrentDutyCyle*1000);
     }, CHANGE_INTERVAL);
   }
 
